c8bbcb8eec4fb1dc65820b3607fecac936392f24
Tiago Barbosa <tiago@farias.cc>
2021-08-12 16:44:44 -0300

    Project created

---
+++ _01_SerializeDeserialize/_01_XMLSerialize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using _00_Library;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_XMLSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User() { Name = "Tiago Barbosa", CPF = "111.111.111-11", Email = "tiago@gmail.com" };
+            var serialize = new XmlSerializer(typeof(User));
+            var stream = new StreamWriter(@"C:\Users\antonio.farias\source\repos\CSharp avancado\Files\01_XMLSerialize.xml");
+
+            serialize.Serialize(stream, user);
+        }
+    }
+}
