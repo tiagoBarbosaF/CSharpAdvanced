928d4fd9af53133160938a352d06f621b981a12d
Tiago Barbosa <tiago@farias.cc>
2021-08-12 17:23:23 -0300

    Serialize and deserialize JSON files

---
+++ _01_SerializeDeserialize/_03_JSONSerialize/Program.cs
@@ -0,0 +1,20 @@
+﻿using _00_Library;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace _03_JSONSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User() { Name = "Rakel Moreira", CPF = "222.222.222-22", Email = "rakel@gmail.com" };
+            var serialize = new JavaScriptSerializer();
+            string serializeObject = serialize.Serialize(user);
+            var sw = new StreamWriter(@"C:\Users\antonio.farias\source\repos\CSharp avancado\Files\02_JSONSerialize.json");
+
+            sw.WriteLine(serializeObject);
+            sw.Close();
+        }
+    }
+}
---
+++ _01_SerializeDeserialize/_04_JSONDeserialize/Program.cs
@@ -0,0 +1,20 @@
+﻿using _00_Library;
+using System;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace _04_JSONDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = new StreamReader(@"C:\Users\antonio.farias\source\repos\CSharp avancado\Files\02_JSONSerialize.json");
+            var fileLines = stream.ReadToEnd();
+            var serialize = new JavaScriptSerializer();
+            var user = (User)serialize.Deserialize(fileLines, typeof(User));
+
+            Console.WriteLine($"User Name: {user.Name}, CPF: {user.CPF}, E-mail: {user.Email}");
+        }
+    }
+}
