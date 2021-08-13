a7023ba2579afcfc78770416e1c25a6bd7049591
Tiago Barbosa <tiago@farias.cc>
2021-08-13 14:39:32 -0300

    Extension methods

---
+++ _02_AdvancedTopics/_03_ExtensionMethods/Lib/Extensions/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ExtensionMethods.Lib.Extensions
+{
+    public static class StringExtension
+    {
+        public static string FirstToUpper(this string str)
+        {
+            var first = str.Substring(0, 1);
+            var second = str.Substring(1);
+
+            return first.ToUpper() + second;
+        }
+    }
+}
---
+++ _02_AdvancedTopics/_03_ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using _03_ExtensionMethods.Lib.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = "hello world!";
+
+            Console.WriteLine(value.FirstToUpper());
+        }
+    }
+}
